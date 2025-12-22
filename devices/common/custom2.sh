bbb6b3d4a95e78ba2b8f5ed4209c9d1e0827826f
gongzi miao <miaogongzi0227@gmail.com>
2025-12-23 01:58:11 +0800

    Update custom2.sh

--- devices/common/custom2.sh
+++ devices/common/custom2.sh
@@ -20,7 +20,6 @@ sed -i '/WARNING: Makefile/d' scripts/package-metadata.pl
 sed -i "s#false; \\\#true; \\\#" include/download.mk
 
 sed -i "s/+\# \$(foreach/+\$(foreach/" devices/common/patches/luci_mk.patch
-sed -i "s/((\$\$1 + 365\*24\*60\*60))/1/" devices/common/patches/luci_mk.patch
 
 cp -f devices/common/po2lmo staging_dir/host/bin/po2lmo
 chmod +x staging_dir/host/bin/po2lmo
