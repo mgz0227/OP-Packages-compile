d48943530244b13f2c3cc7d45f2e08da3e5db7d2
gongzi miao <miaogongzi0227@gmail.com>
2025-12-27 20:20:52 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -28,7 +28,9 @@ rm -Rf feeds/luci/{applications,collections,protocols,themes,libs,docs,contrib}
 rm -Rf feeds/luci/modules/!(luci-base)
 rm -Rf feeds/packages/!(lang|libs|devel|utils|net|multimedia)
 rm -Rf feeds/packages/multimedia/!(gstreamer1)
-rm -Rf feeds/packages/net/!(mosquitto|curl)
+rm -Rf feeds/packages/net/!(mosquitto|curl|unbound)
+rm -Rf feeds/packages/lang/{php*,ruby,perl}
+rm -Rf feeds/packages/utils/!(tar|xz|docker|dockerd|containerd|zstd|unzip|acl|lm-sensors|xxhash|runc|tini)
 rm -Rf feeds/base_root/package/firmware
 rm -Rf feeds/base_root/package/network/!(services|utils)
 rm -Rf feeds/base_root/package/network/services/!(ppp)
